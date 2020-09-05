8e25b702f65305d74a35dfe8ab55d3c7b78ec02c
ShozaburoAbe <61729075+ShozaburoAbe@users.noreply.github.com>
2020-09-05 22:00:37 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <>
+//                 <label htmlFor="bar">bar</label>
+//                 <input type="text" onChange={(event) => console.log(event.target.value)}/>
+//             </>
+//         );
+//     }
+// }
+
 const App = () => {
     return (
-        <>
-            <label htmlFor="bar">bar</label>
-            <input type="text" onChange={(event) => console.log(event.target.value)}/>
-        </>
-  );
+        <div>
+            <Cat />
+            <Cat />
+        </div>
+    )
+};
+
+const Cat = () => {
+    return (
+        <div>Mew</div>
+    );
 };
 
 export default App;
