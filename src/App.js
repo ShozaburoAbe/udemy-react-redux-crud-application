a65452dd684568d94053319f3b50d4dae5467f3e
ShozaburoAbe <61729075+ShozaburoAbe@users.noreply.github.com>
2020-09-06 13:20:05 +0900

    implement action and action creater

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
 const App = () => (
-    <Counter />
-);
+  <Counter/>
+)
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { count: 0 }
-    }
+  constructor (props) {
+    super(props)
+    console.log('super')
+    this.state = { count: 0 }
+  }
 
-    handlePlusButton = () => {
-        this.setState({count: this.state.count + 1 });
-    }
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
 
-    handleMinusButton = () => {
-        this.setState({count: this.state.count - 1 });
-    }
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
 
-    render() {
-        console.log("render")
-        return (
-            <>
-                <div>count: {this.state.count}</div>
-                <button onClick={this.handlePlusButton}>+1</button>
-                <button onClick={this.handleMinusButton}>-1</button>
-            </>
-        );
-    };
+  render () {
+    console.log('render')
+    return (
+      <>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </>
+    )
+  };
 }
 
-export default App;
+export default App
